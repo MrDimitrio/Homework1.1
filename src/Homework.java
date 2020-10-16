949de7dce666f38124c49b259f68a3ef210672d0
Dmitriy Nahotniuk <qa.is.quality@gmail.com>
2020-10-16 14:29:29 +0300

    Initial commit

---
+++ src/Homework.java
@@ -0,0 +1,10 @@
+public class Homework {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("My name is Dmitriy");
+        System.out.println("I have been working as a manual tester for almost two years");
+        System.out.println("I wish everyone productive and interesting learning and professional growth !!!");
+
+    }
+
+}
